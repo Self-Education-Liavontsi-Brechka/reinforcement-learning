8e671422259c0573d982141ddc3c21206b411fda
lbrechka <leontibrechko@gmail.com>
2018-02-14 09:00:47 -0800

    Q-learning

--- topics/td/td_methods.py
+++ topics/td/td_methods.py
@@ -86,3 +86,58 @@ def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
             a = a_prime
 
     return Q, stats
+
+
+def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
+    """
+    Q-Learning algorithm: Off-policy TD control. Finds the optimal greedy policy
+    while following an epsilon-greedy policy
+
+    Args:
+        env: OpenAI environment.
+        num_episodes: Number of episodes to run for.
+        discount_factor: Gamma discount factor.
+        alpha: TD learning rate.
+        epsilon: Chance the sample a random action. Float betwen 0 and 1.
+
+    Returns:
+        A tuple (Q, episode_lengths).
+        Q is the optimal action-value function, a dictionary mapping state -> action values.
+        stats is an EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
+    """
+
+    # The final action-value function.
+    # A nested dictionary that maps state -> (action -> action-value).
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    # Keeps track of useful statistics
+    stats = plotting.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes))
+
+    # The policy we're following
+    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+
+    for i_episode in range(num_episodes):
+        # Print out which episode we're on, useful for debugging.
+        if (i_episode + 1) % 100 == 0:
+            print "\rEpisode {0}/{1}.".format(i_episode + 1, num_episodes)
+            sys.stdout.flush()
+
+        s = env.reset()
+        while True:
+            a_probs = policy(s)
+            a = np.random.choice(np.arange(len(a_probs)), p=a_probs)
+            s_prime, reward, done, info = env.step(a)
+
+            Q[s][a] += alpha * (reward + discount_factor * max(Q[s_prime]) - Q[s][a])
+
+            stats.episode_lengths[i_episode] += 1
+            stats.episode_rewards[i_episode] += reward
+
+            if done:
+                break
+
+            s = s_prime
+
+    return Q, stats
